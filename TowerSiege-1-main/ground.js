b38ff4f656ba9f9f7a31c60cae2fb7fe6b6f6d32
richanar <71360910+richanar@users.noreply.github.com>
2021-05-15 22:07:05 -0400

    Add files via upload

---
+++ TowerSiege-1-main/ground.js
@@ -0,0 +1,16 @@
+class Ground {
+    constructor() {
+      var options = {
+          isStatic: true
+      }
+      this.body = Bodies.rectangle(450,390,900,20,options);
+      
+      World.add(world, this.body);
+    }
+    display(){
+        noStroke()
+      rectMode(CENTER);
+      fill(188,67,67);
+      rect(this.ground.position.x,this.ground.position.y,900,20);
+    }
+  };
